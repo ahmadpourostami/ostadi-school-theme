20b2e077c67981da722a7f0526da8d46a402646d
ahmadpourostami <103726857+ahmadpourostami@users.noreply.github.com>
2026-08-15 23:48:54 +0330

    Add Gutenberg block registration

---
+++ ostadi-elements/includes/class-gutenberg.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+class Ostadi_Elements_Gutenberg {
+    public function __construct() {
+        add_action( 'init', array( $this, 'register_blocks' ) );
+    }
+
+    public function register_blocks() {
+        $blocks = array(
+            'section-heading',
+            'article-card',
+            'article-grid',
+            'category-list',
+        );
+
+        foreach ( $blocks as $block ) {
+            register_block_type( OSTADI_ELEMENTS_DIR . 'blocks/' . $block );
+        }
+    }
+}
