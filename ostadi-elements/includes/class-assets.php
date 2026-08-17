a67e2d36662c2919cf849a4d6eab33d18878867d
ahmadpourostami <103726857+ahmadpourostami@users.noreply.github.com>
2026-08-17 13:38:43 +0330

    Load reference widget styles in frontend and Elementor editor

--- ostadi-elements/includes/class-assets.php
+++ ostadi-elements/includes/class-assets.php
@@ -6,15 +6,15 @@ class Ostadi_Elements_Assets {
         add_action( 'wp_enqueue_scripts', array( $this, 'frontend_assets' ) );
         add_action( 'elementor/editor/after_enqueue_styles', array( $this, 'editor_assets' ) );
     }
-
     public function frontend_assets() {
         wp_enqueue_style( 'ostadi-elements', OSTADI_ELEMENTS_URL . 'assets/css/ostadi-elements.css', array(), OSTADI_ELEMENTS_VERSION );
-        wp_enqueue_style( 'ostadi-home', OSTADI_ELEMENTS_URL . 'assets/css/home.css', array( 'ostadi-elements' ), OSTADI_ELEMENTS_VERSION );
+        wp_enqueue_style( 'ostadi-reference', OSTADI_ELEMENTS_URL . 'assets/css/reference-elements.css', array( 'ostadi-elements' ), OSTADI_ELEMENTS_VERSION );
+        wp_enqueue_style( 'ostadi-home', OSTADI_ELEMENTS_URL . 'assets/css/home.css', array( 'ostadi-reference' ), OSTADI_ELEMENTS_VERSION );
         wp_enqueue_script( 'ostadi-elements-frontend', OSTADI_ELEMENTS_URL . 'assets/js/frontend.js', array(), OSTADI_ELEMENTS_VERSION, true );
     }
-
     public function editor_assets() {
         wp_enqueue_style( 'ostadi-elements-editor', OSTADI_ELEMENTS_URL . 'assets/css/ostadi-elements.css', array(), OSTADI_ELEMENTS_VERSION );
-        wp_enqueue_style( 'ostadi-home-editor', OSTADI_ELEMENTS_URL . 'assets/css/home.css', array( 'ostadi-elements-editor' ), OSTADI_ELEMENTS_VERSION );
+        wp_enqueue_style( 'ostadi-reference-editor', OSTADI_ELEMENTS_URL . 'assets/css/reference-elements.css', array( 'ostadi-elements-editor' ), OSTADI_ELEMENTS_VERSION );
+        wp_enqueue_style( 'ostadi-home-editor', OSTADI_ELEMENTS_URL . 'assets/css/home.css', array( 'ostadi-reference-editor' ), OSTADI_ELEMENTS_VERSION );
     }
 }
