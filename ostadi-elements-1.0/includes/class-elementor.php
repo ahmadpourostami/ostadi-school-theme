cc0b3114d786bcda6ea1f283c0e2874f17269846
ahmadpourostami <103726857+ahmadpourostami@users.noreply.github.com>
2026-08-17 14:30:32 +0330

    Add Ostadi Elementor integration

---
+++ ostadi-elements-1.0/includes/class-elementor.php
@@ -0,0 +1,7 @@
+<?php
+if(!defined('ABSPATH'))exit;
+class Ostadi_Elements_Elementor{
+ public function __construct(){add_action('elementor/elements/categories_registered',[$this,'cat']);add_action('elementor/widgets/register',[$this,'widgets']);}
+ public function cat($m){$m->add_category('ostadi-1',['title'=>'المان‌های استادی ۱.۰','icon'=>'fa fa-graduation-cap']);}
+ public function widgets($m){if(!class_exists('\\Elementor\\Widget_Base'))return;require_once OSTADI_ELEMENTS_DIR.'elementor/class-reference-showcase.php';$m->register(new Ostadi_Elementor_Reference_Showcase());}
+}
